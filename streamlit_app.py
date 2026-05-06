cb1a9eca15c19b011b4fbddf823ed1d1e6f3e25a
Otilia25 <omuyabi@gmail.com>
2026-05-06 12:04:57 +0200

    Remove Foundation Farm app from the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ apps = [
     {"func": ce_app.app, "title": "C&E", "icon": ":seedling:"},
     {"func": sh_app.app, "title": "Small Holder", "icon": ":seedling:"},
     {"func": soil_app.app, "title": "Analysis", "icon": ":seedling:"},
-    {"func": ff_app.app, "title": "Foundation Farm", "icon": ":leaf:"},
     {"func": hb_app.app, "title": "Hub Definition", "icon": ":seedling:"},
     {"func": fs_app.app, "title": "FCA", "icon": ":seedling:"},
 ]
